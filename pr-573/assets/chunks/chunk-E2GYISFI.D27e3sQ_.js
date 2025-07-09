704a99f2f08b7318480a6b20e4d6bc1a766d127c
pthmas <pthmas@users.noreply.github.com>
2025-07-09 12:01:47 +0000

    Deploy preview for PR 573 🛫

---
+++ pr-573/assets/chunks/chunk-E2GYISFI.D27e3sQ_.js
@@ -0,0 +1,15 @@
+import{_ as e}from"../app.Dxk4JVAx.js";var l=e(()=>`
+  /* Font Awesome icon styling - consolidated */
+  .label-icon {
+    display: inline-block;
+    height: 1em;
+    overflow: visible;
+    vertical-align: -0.125em;
+  }
+  
+  .node .label-icon path {
+    fill: currentColor;
+    stroke: revert;
+    stroke-width: revert;
+  }
+`,"getIconStyles");export{l as g};
